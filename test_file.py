56e0876aed00f2546b274b8e3a8f42a1d0ad47c8
Sophia Jannetty <skjannetty@gmail.com>
2018-02-10 02:57:18 -0500

    We did a thing

--- test_file.py
+++ test_file.py
@@ -1,1 +1,35 @@
-print("Hello World!")
+import fileinput
+
+def search_and_enter(phonenum, newtext):
+    filename = "text_file.txt"
+    file = open(filename, "r")
+    linefile = open("linefile.txt", "w+")
+    num = 1
+    for num, line in enumerate(file, 1):
+        print(num)
+        #if line == :
+        #    print("after the if")
+        #    open(filename, "a")
+        #    file.write(phonenum)
+        #    file.write(newtext)
+        if phonenum in line:
+            break
+    file.close()
+    file = open(filename, "r")
+    for x in range (0, num-1):
+        linefile.write(file.readline())
+    newtext = file.readline().rstrip() + ", " + newtext;
+    linefile.write(newtext)
+    for line in file:
+        linefile.write(file.readline())
+    linefile.close()
+    file.close()
+
+def main():
+    phonenum = "8675309"
+    newtext = "Jeremy"
+    #phonenum, newtext = tuple
+    search_and_enter(phonenum, newtext)
+
+if __name__ == "__main__":
+    main()
